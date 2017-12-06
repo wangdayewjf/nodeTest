96a82088c179d6f5d2bbdd95b3c1248003279f1d
wangjianfeng <wang.jianfeng@yahoo.com>
2017-12-06 17:42:55 +0800

    add firstTestDemo

---
+++ app.js
@@ -0,0 +1,23 @@
+var through2  = require('through2');
+var concat = require('concat-stream');
+var duplex = require('./testModules/duplex');
+var stream = through2(write,end)
+process.stdin
+    .pipe(stream)
+    .pipe(duplex)
+    .pipe(process.stdout);
+
+function write(line,_,next){
+
+    this.push(line.toString().toUpperCase())
+    next();
+}
+function end(done){
+    done();
+}
+
+
+var reverseStream=concat(function(text){
+	console.log('reverseStream');
+    
+})
